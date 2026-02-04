cfa38b3e203ebd387119fbb56575a84866b3a06a
MuhammadMaazSultan <muhammadmaazsultan11133@gmail.com>
2026-02-04 11:27:10 +0500

    Initial commit

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
 EMBEDDING_MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
 CHAT_MODEL_ID = 'Qwen/Qwen2.5-1.5B-Instruct'
-TEMPLATE = ''' Answer the query only from the following context if the context is insufficient or answer is not available in context just say you don't know
-            context:
-            {context}\n
-
-            question:
-            {question}''',
